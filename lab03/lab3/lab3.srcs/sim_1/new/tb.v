14f7e21a57c6c6ee4f2ab02e5c286564f5fe91ac
lgm0905 <lgm0905@snu.ac.kr>
2021-03-20 22:56:29 +0900

    Update lab03

---
+++ lab03/lab3/lab3.srcs/sim_1/new/tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/03/20 15:49:04
+// Design Name: 
+// Module Name: tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb(
+
+    );
+    
+    my_add MY_ADDER();
+    
+endmodule
