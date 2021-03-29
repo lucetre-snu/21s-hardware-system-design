488fe2864688586e55236da7718661b1b1a146ac
LUCETRE\lucetre <lucetre@github.com>
2021-03-29 14:43:44 +0900

    Start lab04

---
+++ lab03/src/my_add.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module my_add #(
+    parameter BITWIDTH = 32
+)
+(
+    input [BITWIDTH-1:0] ain,
+    input [BITWIDTH-1:0] bin,
+    output [BITWIDTH-1:0] dout,
+    output overflow
+);
+
+    /* IMPLEMENT HERE! */
+    // concatnate (overflow, dout) & detect overflow
+    assign {overflow, dout} = ain + bin;
+
+endmodule
