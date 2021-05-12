a8c68ec2838f3712b65cdf114d948e67cf459c9c
lucetre <lucetre@naver.com>
2021-05-12 18:51:02 +0900

    Log sth

--- lab08/hsd21_lab08_practice/main.c
+++ lab08/hsd21_lab08_practice/main.c
@@ -28,6 +28,7 @@ int main(int argc, char** argv)
   // run ip
   *(fpga_ip) = 0x5555;
   while (*fpga_ip == 0x5555);
+  printf("%X", *fpga_ip);
 
   printf("%-10s%-10s\n", "addr", "FPGA(hex)");
   for (i = 0; i < SIZE * 2; i++)
