c9e79754851f05b0cdeb5acfca4dd8acf86e8a06
lucetre <lucetre@naver.com>
2021-05-28 17:25:20 +0900

    Refine MM multiplication

--- Project/Project_V0_interim/mm_mul/mm_mul.srcs/sim_1/new/tb_mm_multiplier.v
+++ Project/Project_V0_interim/mm_mul/mm_mul.srcs/sim_1/new/tb_mm_multiplier.v
@@ -3,41 +3,50 @@
 module tb_mm_multiplier #(
     parameter L_RAM_SIZE = 3,
     parameter BITWIDTH = 32,
-    parameter FILENAME = "global_buffer_mm.txt"
+    parameter INFILE = "global_buffer_in.txt",
+    parameter OUTFILE = "global_buffer_out.txt"
 )();
     localparam MATRIX_SIZE = 2**(L_RAM_SIZE*2);
     localparam VECTOR_SIZE = 2**(L_RAM_SIZE);
-    reg [BITWIDTH-1:0] gb[0:MATRIX_SIZE*2-1];
+    reg [BITWIDTH-1:0] rdgb[0:MATRIX_SIZE*2-1];
+    reg [BITWIDTH-1:0] wrgb[0:MATRIX_SIZE-1];
     wire [BITWIDTH-1:0] rddata;
+    wire [BITWIDTH-1:0] wrdata;
     wire [2*L_RAM_SIZE:0] rdaddr;
-    wire done;
+    wire [2*L_RAM_SIZE:0] wraddr;
+    wire done, we;
     
     reg start, clk, reset;
 //    integer i;
 //    initial begin
 //        for(i = 0; i < MATRIX_SIZE; i = i+1) begin
-//            gb[i]               = $urandom_range(2**30, 2**30+2**24);
-//            gb[MATRIX_SIZE + i] = $urandom_range(2**30, 2**30+2**24);
+//            rdgb[i]               = $urandom_range(2**30, 2**30+2**24);
+//            rdgb[MATRIX_SIZE + i] = $urandom_range(2**30, 2**30+2**24);
 //        end
-//        $writememh("global_buffer_mm.txt", gb);
+//        $writememh(INFILE, rdgb);
 //    end
-    
-    assign rddata = start ? gb[rdaddr] : 0;
+    assign rddata = start ? rdgb[rdaddr] : 0;
     initial begin
-        $readmemh(FILENAME, gb);
+        $readmemh(INFILE, rdgb);
         clk <= 0;
         start <= 0; reset <= 1;
         #10 start <= 1; reset <= 0;
     end
-    always #1 clk = ~clk;
+    always @(*)
+        if (we) wrgb[wraddr] = wrdata;
+    always @(posedge done)
+        $writememh(OUTFILE, wrgb);
     
+    always #1 clk = ~clk;
     mm_multiplier #(L_RAM_SIZE, BITWIDTH) MM_MULTIPLIER(
         .start(start),
         .reset(reset),
         .clk(clk),
         .rdaddr(rdaddr),
         .rddata(rddata),
+        .we(we),
+        .wraddr(wraddr),
+        .wrdata(wrdata),
         .done(done)
     );
-
 endmodule
--- Project/Project_V0_interim/mm_mul/mm_mul.srcs/sources_1/new/mm_multiplier.v
+++ Project/Project_V0_interim/mm_mul/mm_mul.srcs/sources_1/new/mm_multiplier.v
@@ -8,22 +8,26 @@ module mm_multiplier #(
     input reset,
     input clk,
     output [2*L_RAM_SIZE:0] rdaddr,
+    output [2*L_RAM_SIZE:0] wraddr,
     input [BITWIDTH-1:0] rddata,
+    output [BITWIDTH-1:0] wrdata,
+    output we,
     output done
 );
     localparam DONE_LATENCY = 5;
-    localparam S_IDLE = 2'd0, S_LOAD = 2'd1, S_CALC = 2'd2, S_DONE = 2'd3;
+    localparam S_IDLE = 3'd0, S_LOAD = 3'd1, S_CALC = 3'd2, S_HARV = 3'd3, S_DONE = 3'd4;
     localparam MATRIX_SIZE = 2**(L_RAM_SIZE*2);
     localparam VECTOR_SIZE = 2**(L_RAM_SIZE);
     
-    reg [1:0] present_state, next_state;
-    reg [2*L_RAM_SIZE:0] cnt_load;
+    reg [2:0] present_state, next_state;
+    reg [2*L_RAM_SIZE:0] cnt_load, cnt_harv;
     reg [L_RAM_SIZE:0] cnt_calc;
     reg [2:0] cnt_done;
-    reg rst_cnt_load, rst_cnt_calc, rst_cnt_done;
+    reg rst_cnt_load, rst_cnt_calc, rst_cnt_harv, rst_cnt_done;
     
     reg [BITWIDTH-1:0] gb1[0:MATRIX_SIZE-1];
     reg [BITWIDTH-1:0] gb2[0:MATRIX_SIZE-1];
+    reg [BITWIDTH-1:0] data[0:MATRIX_SIZE-1];
     
     reg [BITWIDTH-1:0] ain[0:VECTOR_SIZE-1];
     reg [BITWIDTH-1:0] bin[0:VECTOR_SIZE-1];
@@ -35,11 +39,12 @@ module mm_multiplier #(
     
     always @(posedge clk or posedge reset)
         if (reset) present_state <= S_IDLE; else present_state <= next_state;
-        
     always @(posedge clk or posedge rst_cnt_load) 
         if (rst_cnt_load) cnt_load <= 0; else cnt_load <= cnt_load + 1;
     always @(posedge clk or posedge rst_cnt_calc) 
         if (rst_cnt_calc) cnt_calc <= 0;
+    always @(posedge clk or posedge rst_cnt_harv) 
+        if (rst_cnt_harv) cnt_harv <= 0; else cnt_harv <= cnt_harv + 1;
     always @(posedge clk or posedge rst_cnt_done) 
         if (rst_cnt_done) cnt_done <= 0; else cnt_done <= cnt_done + 1;
     
@@ -47,7 +52,8 @@ module mm_multiplier #(
         case (present_state)
             S_IDLE: if (start)                          next_state = S_LOAD; else next_state = present_state;
             S_LOAD: if (cnt_load == MATRIX_SIZE*2-1)    next_state = S_CALC; else next_state = present_state;
-            S_CALC: if (cnt_calc == VECTOR_SIZE)        next_state = S_DONE; else next_state = present_state;
+            S_CALC: if (cnt_calc == VECTOR_SIZE)        next_state = S_HARV; else next_state = present_state;
+            S_HARV: if (cnt_harv == MATRIX_SIZE-1)      next_state = S_DONE; else next_state = present_state;
             S_DONE: if (cnt_done == DONE_LATENCY-1)     next_state = S_IDLE; else next_state = present_state;
         endcase
     
@@ -55,39 +61,48 @@ module mm_multiplier #(
         case (present_state)
             S_LOAD: rst_cnt_load <= 0;
             S_CALC: rst_cnt_calc <= 0;
+            S_HARV: rst_cnt_harv <= 0;
             S_DONE: rst_cnt_done <= 0;
             default: begin
                 rst_cnt_load <= 1; 
                 rst_cnt_calc <= 1;
+                rst_cnt_harv <= 1;
                 rst_cnt_done <= 1;
             end
         endcase
         
     always @(rddata or present_state)
-        if (present_state == S_LOAD) begin
-            if (cnt_load < MATRIX_SIZE) gb1[cnt_load] = rddata; else gb2[cnt_load-MATRIX_SIZE] = rddata;
-        end
+        if (present_state == S_LOAD) 
+            if (cnt_load < MATRIX_SIZE) gb1[cnt_load]             = rddata; 
+            else                        gb2[cnt_load-MATRIX_SIZE] = rddata;
+        
+    integer j;
+    always @(present_state)
+        if (present_state == S_HARV)
+            for (j = 0; j < MATRIX_SIZE; j = j+1)
+                data[j] <= out[j];
     
-    integer rc;
     always @(dvalid or present_state)
-        if (present_state == S_CALC) begin
+        if (present_state == S_CALC)
             if (dvalid) begin
                 cnt_calc <= cnt_calc + 1;
                 valid <= 0;
             end
             else begin
-                for (rc = 0; rc < VECTOR_SIZE; rc = rc+1) begin
-                    ain[rc] <= gb1[rc*VECTOR_SIZE + cnt_calc];
-                    bin[rc] <= gb2[cnt_calc*VECTOR_SIZE + rc];
+                for (j = 0; j < VECTOR_SIZE; j = j+1) begin
+                    ain[j] <= gb1[j*VECTOR_SIZE + cnt_calc];
+                    bin[j] <= gb2[cnt_calc*VECTOR_SIZE + j];
                 end
                 valid <= 1;
             end
-        end
+    
+    assign rdaddr = (present_state == S_LOAD) ? cnt_load : 0;
+    assign wrdata = (present_state == S_HARV) ? data[cnt_harv] : 0;
+    assign wraddr = (present_state == S_HARV) ? cnt_harv : 0;
+    assign we     = (present_state == S_HARV);
+    assign done   = (present_state == S_DONE);
     
     genvar i;
-    assign rdaddr = present_state == S_LOAD ? cnt_load : 0;
-    assign done = present_state == S_DONE ? 1 : 0;
-    
     generate for (i = 0; i < MATRIX_SIZE; i = i+1) begin: MATRIX
         my_pe #(L_RAM_SIZE, BITWIDTH) MY_PE(
             .aclk(clk),
@@ -98,7 +113,6 @@ module mm_multiplier #(
             .dvalid(dvalid),
             .dout(dout[i])
         );
-        assign out[i] = present_state == S_DONE ? dout[i] : 0;
+        assign out[i] = present_state == S_HARV ? dout[i] : 0;
     end endgenerate
-    
 endmodule
