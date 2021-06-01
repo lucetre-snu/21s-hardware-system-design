7b76c981c69ea574bc50a77e88544da78b95e34e
lucetre <lucetre@naver.com>
2021-06-01 17:23:06 +0900

    Fix main.cpp

--- lab11/src/hsd21_lab11/main.c
+++ lab11/src/hsd21_lab11/main.c
@@ -12,7 +12,7 @@ typedef union {
   unsigned int i;
 } foo;
 
-struct timeval st[4];
+struct timeval st[7];
 int st2time (struct timeval st_) {
   return st_.tv_sec * 1000 * 1000 + st_.tv_usec;
 }
@@ -42,6 +42,7 @@ int main(int argc, char** argv) {
   int foo;
   foo = open("/dev/mem", O_RDWR);
   float *ps_dram = mmap(NULL, (SIZE * (SIZE + 1))* sizeof(float), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x10000000);
+  float *fpga_bram = mmap(NULL, (SIZE * (SIZE + 1))* sizeof(float), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x40000000);
 
   // MCPY: DRAM -> DRAM
   gettimeofday (&st[0], NULL);
@@ -51,30 +52,41 @@ int main(int argc, char** argv) {
   memcpy( ps_dram, flat, SIZE * (SIZE + 1) * sizeof(float) );
   gettimeofday (&st[2], NULL);
 
+
   // DMA : DRAM -> BRAM
   unsigned int *fpga_dma = mmap(NULL, 16*sizeof(unsigned int), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x7E200000);
-  gettimeofday (&st[2], NULL);
+  gettimeofday (&st[3], NULL);
   *(fpga_dma+6) = 0x10000000;
   *(fpga_dma+8) = 0xC0000000;
   *(fpga_dma+10) = SIZE * (SIZE + 1) * sizeof(float);
   while ((*(fpga_dma+1) & 0x00000002) == 0);
-  gettimeofday (&st[3], NULL);
+  gettimeofday (&st[4], NULL);
 
   // Check if the data is valid
-  float *fpga_bram = mmap(NULL, (SIZE * (SIZE + 1))* sizeof(float), PROT_READ|PROT_WRITE, MAP_SHARED, foo, 0x40000000);
   int num_mismatch = 0;
-  for (i = 0; i < SIZE * (SIZE + 1); i++) {
-    if ( *(fpga_bram + i) != *(ps_dram + i) ) {
-      printf("%f, %f\n", *(fpga_bram + i), *(ps_dram + i) );
+  for (i = 0; i < SIZE * (SIZE + 1); i++)
+    if ( *(fpga_bram + i) != *(ps_dram + i) )
       num_mismatch++;
-    }
-  }
+  printf("The number of mismatch (DMA dram <-> bram) : %d\n", num_mismatch);
+
+
+  // CPU : DRAM -> BRAM
+  gettimeofday (&st[5], NULL);
+  memcpy( ps_dram, fpga_bram, SIZE * (SIZE + 1) * sizeof(float) );
+  gettimeofday (&st[6], NULL);
   
-  printf("The number of mismatch (dram <-> bram) : %d\n", num_mismatch);
+  // Check if the data is valid
+  int num_mismatch = 0;
+  for (i = 0; i < SIZE * (SIZE + 1); i++)
+    if ( *(fpga_bram + i) != *(ps_dram + i) )
+      num_mismatch++;
+  printf("The number of mismatch (CPU dram <-> bram) : %d\n", num_mismatch);
+
 
   printf("%s: %d\n", "MCPY1", st2time(st[ 1]) - st2time(st[ 0]));
   printf("%s: %d\n", "MCPY2", st2time(st[ 2]) - st2time(st[ 1]));
-  printf("%s: %d\n", "DMA  ", st2time(st[ 3]) - st2time(st[ 2]));
+  printf("%s: %d\n", "DMA  ", st2time(st[ 4]) - st2time(st[ 3]));
+  printf("%s: %d\n", "CPU  ", st2time(st[ 6]) - st2time(st[ 5]));
 
   close(foo);
 
