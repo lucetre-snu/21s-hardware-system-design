e918bc9b293d4897db55ae157e5a90bf52df8c6c
lucetre <lucetre@naver.com>
2021-06-11 23:18:37 +0900

    Debugging

--- Project/Test/hsd21_add01_checkhw/main.cpp
+++ Project/Test/hsd21_add01_checkhw/main.cpp
@@ -21,8 +21,8 @@ int main(void)
 	for(int i = 0 ; i < data_size_M*2 ; ++i)
 		flat[i] = 1;
 	for (int i = 0; i < 3; i++) {
-		mat[v_size*7+i] = 0;
-		input[v_size*i+7] = 0;
+		mat[v_size*i+7] = 0;
+		input[v_size*7+i] = 0;
 	}
 	/*
 	for (int i = 0; i < 8; i++) {
