2fca4d3a3f96560be2567e4bc94e522ea1ad8b76
lucetre <lucetre@naver.com>
2021-06-12 12:05:30 +0900

    Fix some

--- Project/proj/V0/Software/hsd21_project/test.py
+++ Project/proj/V0/Software/hsd21_project/test.py
@@ -55,8 +55,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Neural Network Accleration on FPGA')
     parser.add_argument('--num_test_images', type=int, default=100, help='The number of test images (range: 1~10000)')
-    parser.add_argument('--m_size', type=int, default=16, help='The number of row in the block operation')
-    parser.add_argument('--v_size', type=int, default=16, help='The number of col in the block operation')
+    parser.add_argument('--m_size', type=int, default=8, help='The number of row in the block operation')
+    parser.add_argument('--v_size', type=int, default=8, help='The number of col in the block operation')
     parser.add_argument('--run_type', type=str, default='cpu', help='The type of execution e.g. cpu, fpga')
     parser.add_argument('--network', type=str, default='cnn', help='The type of execution e.g. cnn, mlp')
 
