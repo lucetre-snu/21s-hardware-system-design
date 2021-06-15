f60924c3db2e09a313d6eababf835b6d652787a6
lucetre <lucetre@naver.com>
2021-06-15 16:55:14 +0900

    print noncache

--- Project/proj/V1_DMA/Software/hsd21_add01_checkhw/src/fpga_api.cpp
+++ Project/proj/V1_DMA/Software/hsd21_add01_checkhw/src/fpga_api.cpp
@@ -60,7 +60,7 @@ const float* __attribute__((optimize("O0"))) FPGA::run()
     transfer(noncache_addr, bram_addr, DATA_SIZE);
 
     for (int i = 0; i < 128; i++) {
-      cout << data_[i] << " ";
+      cout << data_noncache[i] << " ";
       if (i % 8 == 7) cout << endl;
     }
 
@@ -68,7 +68,7 @@ const float* __attribute__((optimize("O0"))) FPGA::run()
     while(*api_ == 0x5555);
     transfer(bram_addr, noncache_addr, DATA_SIZE);
     memcpy(data_noncache, data_, DATA_SIZE); 
-    for (int i = 0; i < 128; i++) {
+    for (int i = 0; i < 64; i++) {
       cout << data_[i] << " ";
       if (i % 8 == 7) cout << endl;
     }
