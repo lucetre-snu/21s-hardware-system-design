a662f79283630b94bae7ed3788d72751d600c539
lucetre <lucetre@naver.com>
2021-06-15 14:22:00 +0900

    add

--- Project/proj/V1_DMA/Software/hsd21_add01_checkhw/src/fpga_api.cpp
+++ Project/proj/V1_DMA/Software/hsd21_add01_checkhw/src/fpga_api.cpp
@@ -52,19 +52,23 @@ void __attribute__((optimize("O0"))) FPGA::transfer(const float *src, const floa
 
 const float* __attribute__((optimize("O0"))) FPGA::run()
 {
-    memcpy(data_noncache, data_, DATA_SIZE); 
+    for (int i = 0; i < 128; i++) {
+      cout << noncache_addr[i] << " ";
+      if (i % 8 == 7) cout << endl;
+    }
+    memcpy(data_, data_noncache, DATA_SIZE); 
     transfer(noncache_addr, bram_addr, DATA_SIZE);
     for (int i = 0; i < 128; i++) {
-      cout << data_[i] << " ";
+      cout << noncache_addr[i] << " ";
       if (i % 8 == 7) cout << endl;
     }
 
     *api_ = 0x5555;
     while(*api_ == 0x5555);
-    transfer(bram_addr, noncache_addr, DATA_SIZE/2);
-    memcpy(data_, data_noncache, DATA_SIZE); 
+    transfer(bram_addr, noncache_addr, DATA_SIZE);
+    memcpy(data_noncache, data_, DATA_SIZE); 
     for (int i = 0; i < 64; i++) {
-      cout << data_[i] << " ";
+      cout << noncache_addr[i] << " ";
       if (i % 8 == 7) cout << endl;
     }
 
