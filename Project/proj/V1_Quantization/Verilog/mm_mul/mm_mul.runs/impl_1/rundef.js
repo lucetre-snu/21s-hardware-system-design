bf00750dfaa7e37dccaffa6938414322d3752011
이지원 <lgm0905@my-debian2>
2021-06-17 22:27:38 +0900

    Quant push

--- Project/proj/V1_Quantization/Verilog/mm_mul/mm_mul.runs/impl_1/rundef.js
+++ Project/proj/V1_Quantization/Verilog/mm_mul/mm_mul.runs/impl_1/rundef.js
@@ -4,13 +4,17 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 //
 
+echo "This script was generated under a different operating system."
+echo "Please update the PATH variable below, before executing this script"
+exit
+
 var WshShell = new ActiveXObject( "WScript.Shell" );
 var ProcEnv = WshShell.Environment( "Process" );
 var PathVal = ProcEnv("PATH");
 if ( PathVal.length == 0 ) {
-  PathVal = "C:/Xilinx/SDK/2018.3/bin;C:/Xilinx/Vivado/2018.3/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2018.3/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2018.3/bin;";
+  PathVal = "/csehome/lgm0905/tools/SDK/2018.3/bin:/csehome/lgm0905/tools/Vivado/2018.3/ids_lite/ISE/bin/lin64;/csehome/lgm0905/tools/Vivado/2018.3/ids_lite/ISE/lib/lin64;/csehome/lgm0905/tools/Vivado/2018.3/bin;";
 } else {
-  PathVal = "C:/Xilinx/SDK/2018.3/bin;C:/Xilinx/Vivado/2018.3/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2018.3/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2018.3/bin;" + PathVal;
+  PathVal = "/csehome/lgm0905/tools/SDK/2018.3/bin:/csehome/lgm0905/tools/Vivado/2018.3/ids_lite/ISE/bin/lin64;/csehome/lgm0905/tools/Vivado/2018.3/ids_lite/ISE/lib/lin64;/csehome/lgm0905/tools/Vivado/2018.3/bin;" + PathVal;
 }
 
 ProcEnv("PATH") = PathVal;
