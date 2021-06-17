8017e975bc0b900f0f105c0eb1468136c0fa4ea4
lucetre <lucetre@naver.com>
2021-06-17 20:59:51 +0900

    hello

--- Project/proj/V1_Quantization/Software/hsd21_project_quant/src/fpga_api.cpp
+++ Project/proj/V1_Quantization/Software/hsd21_project_quant/src/fpga_api.cpp
@@ -37,8 +37,8 @@ FPGA::FPGA(off_t data_addr, off_t output_addr, int m_size, int v_size)
 
   qdata_ = new int[data_size_];
   qdata_M = new int[data_size_M];
-  // qdata_M = static_cast<int *>(mmap(NULL, data_size_M, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, data_addr));
-  // output_ = static_cast<unsigned int *>(mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_, output_addr));
+  qdata_M = static_cast<int *>(mmap(NULL, data_size_M, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, data_addr));
+  output_ = static_cast<unsigned int *>(mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_, output_addr));
   num_block_call_ = 0;
 }
 
