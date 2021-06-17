d47c0bfef4cf75b1ac27d606d1e3ff7e98f5ec8f
lucetre <lucetre@naver.com>
2021-06-17 16:05:22 +0900

    j

--- Project/proj/V1_Quantization/Software/hsd21_project_quant/benchmark.sh
+++ Project/proj/V1_Quantization/Software/hsd21_project_quant/benchmark.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Lab9 - Conv Lowering(CPU) - Acc: 1.0
-sudo python eval.py --num_test_images 1000 --m_size 64 --v_size 64 --network cnn --run_type cpu
+sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network cnn --run_type cpu
 # echo -e '\n=> Accuracy should be 1.0\n'
 
 # Quantization - Acc: 1.0
-sudo python eval.py --num_test_images 1000 --m_size 64 --v_size 64 --network cnn --run_type cpu --quantized
+sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network cnn --run_type cpu --quantized
 # echo -e '\n=> Accuracy should be 1.0\n'
 
 # Projec optimized version
