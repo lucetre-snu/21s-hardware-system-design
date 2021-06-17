33d5da38a70ab7880b54fe48cbabb0633f7b2c3e
lucetre <lucetre@naver.com>
2021-06-17 21:06:06 +0900

    Ready to implement FPGA

--- Project/proj/V1_Quantization/Software/hsd21_project_quant/src/fpga_api.cpp
+++ Project/proj/V1_Quantization/Software/hsd21_project_quant/src/fpga_api.cpp
@@ -28,6 +28,7 @@ FPGA::FPGA(off_t data_addr, off_t output_addr, int m_size, int v_size)
   qout_M = new int[v_size_*v_size_];
 
   output_ = new unsigned int[m_size_]; // use output_ as tempolar output
+  // output_ = static_cast<unsigned int *>(mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_, output_addr));
   output_M = new unsigned int[v_size_*v_size_]; // use output_M as tempolar output
 
   data_ = new float[data_size_];
@@ -38,7 +39,6 @@ FPGA::FPGA(off_t data_addr, off_t output_addr, int m_size, int v_size)
   qdata_ = new int[data_size_];
   qdata_M = new int[data_size_M];
   // qdata_M = static_cast<int *>(mmap(NULL, data_size_M, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, data_addr));
-  // output_ = static_cast<unsigned int *>(mmap(NULL, sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_, output_addr));
   num_block_call_ = 0;
 }
 
@@ -184,6 +184,12 @@ const float* FPGA::blockMM(Compute* comp)
           qout_M[v_size_*i+j] += qm1_[v_size_*i+k] * qm2_[v_size_*k+j];
       }
     }
+    
+    // memcpy(qdata_M, qm1, sizeof(int)*m1_size_);
+    // memcpy(qdata_M + m1_size_, qm2, sizeof(int)*m2_size_);
+    // qblockMM(comp);
+    // memcpy(qout_M, qdata_M, sizeof(float)*m1_size_);
+    
     dequantize(qout_M, out, m1_size_, offset, weight_scale*act_scale);
   }
   else{
